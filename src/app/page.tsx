f3c64f90e0961408f8a82056d99000b396d765d7
Roy Lopez <roylopezdev@gmail.com>
2024-03-20 11:30:03 -0300

    Refactor page layout and add navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,21 @@ import backgroundMinImg from "../../public/assets/UX_UI_background.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
-      <div className="pt-14 pl-10">
-        <h1 className="text-[128px]">Yo</h1>
-        <h1 className="text-[170px] uppercase">Soy</h1>
-        <Image
-          src={backgroundMinImg}
-          alt="Bakcground Image"
-          width={924}
-          height={1080}
-          className="absolute top-96 left-52 z-[-1]"
-        />
-      </div>
+    <main className="flex min-h-screen flex-col relative">
+      <section>
+        <div className="pt-14 pl-10">
+          <h1 className="text-[128px]">Yo</h1>
+          <h1 className="text-[170px] uppercase">Soy</h1>
+          <nav></nav>
+          <Image
+            src={backgroundMinImg}
+            alt="Bakcground Image"
+            width={924}
+            height={1080}
+            className="absolute top-96 left-52 z-[-1]"
+          />
+        </div>
+      </section>
     </main>
   );
 }
