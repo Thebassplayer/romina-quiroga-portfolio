74a33f94a3ea1d874d087141b218c9c674f508d8
Roy Lopez <roylopezdev@gmail.com>
2024-03-20 14:04:24 -0300

    Update page layout and add Romi image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,44 @@
 import Image from "next/image";
 import backgroundMinImg from "../../public/assets/UX_UI_background.png";
+import romi from "../../public/assets/romi.png";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col relative">
-      <section>
-        <div className="pt-14 pl-10">
+    <main className="flex min-h-screen flex-col">
+      <section className="relative">
+        <div className="pl-10 pt-10">
           <h1 className="text-[128px]">Yo</h1>
           <h1 className="text-[170px] uppercase">Soy</h1>
-          <nav></nav>
+        </div>
+        <div className="absolute right-96 top-11 -translate-x-28">
+          <button className="rounded-full  border-2 border-secondary bg-primary px-6 py-1 text-3xl text-secondary hover:bg-primarylight">
+            Ingles
+          </button>
+        </div>
+        <nav className="fixed right-4 top-8 flex flex-row gap-8">
+          <button className="rounded-full border-4 border-secondary bg-primary px-10 py-2 text-5xl text-secondary hover:bg-primarylight">
+            Soy
+          </button>
+          <button className="rounded-full border-4 border-black bg-secondary px-10 py-2 text-5xl text-primary hover:bg-secondarylight">
+            Trabajo
+          </button>
+        </nav>
+        <div className="relative w-full">
           <Image
             src={backgroundMinImg}
             alt="Bakcground Image"
-            width={924}
+            width={1300}
             height={1080}
-            className="absolute top-96 left-52 z-[-1]"
+            className="relative -top-28 left-1/2 -z-10 -translate-x-1/2"
           />
         </div>
+      </section>
+      <section className="bg-sky-background relative h-screen bg-cover">
+        <Image
+          src={romi}
+          alt="Romi"
+          className="absolute left-0 top-0 h-full w-auto"
+        />
       </section>
     </main>
   );
