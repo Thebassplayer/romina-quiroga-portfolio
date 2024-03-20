cc3ae47e0e586900bd24ba07ff78fd518a844d09
Roy Lopez <roylopezdev@gmail.com>
2024-03-20 14:04:18 -0300

    Add cx utility function to apply class names

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import cx from "./utils/classesReducer";
 
 const roboto = Roboto({
   weight: "700",
@@ -20,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={cx(roboto.className)}>{children}</body>
     </html>
   );
 }
