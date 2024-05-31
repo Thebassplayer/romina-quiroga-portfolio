34b0af3a6b9c929c76e01aa314043a65c20065ee
Roy Lopez <ryhsu2@gmail.com>
2024-05-31 14:59:26 -0300

    feat: Add DesktopMenu component for navigation
    Add a new component called DesktopMenu to the layout, which provides navigation options for About, Contact, and Proyects pages. Each option is represented by an icon and a title. The component is rendered in the layout and replaces the previous static navigation menu. This change improves the user experience by providing a more interactive and visually appealing navigation menu.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import cx from "./utils/classesReducer";
+import DesktopMenu from "@/components/DesktopMenu/DesktopMenu";
 
 const roboto = Roboto({
   weight: "400",
@@ -25,14 +26,7 @@ export default function RootLayout({
         className={cx("overflow-y-auto bg-romi-gradient bg-cover bg-center")}
       >
         <main className="relative flex flex-col">
-          <nav className="absolute top-0 w-screen">
-            <ul className="flex w-full flex-row justify-evenly px-80 py-16 font-SansationLg">
-              <li className="text-2xl">Proyectos</li>
-              <li className="text-2xl">Sobre Mi</li>
-              <li className="text-2xl">Email</li>
-              <li className="text-2xl">Ingles</li>
-            </ul>
-          </nav>
+          <DesktopMenu />
           {children}
         </main>
       </body>
--- src/components/DesktopMenu/DesktopMenu.tsx
+++ src/components/DesktopMenu/DesktopMenu.tsx
@@ -1,29 +1,41 @@
-import path from "path";
+"use client";
 import React from "react";
+import Fingerprint from "../Icons/Fingerprint";
+import EmailIcon from "../Icons/EmailIcon";
+import AboutIcon from "../Icons/AboutIcon";
+import LanguajeIcon from "../Icons/languajeIcon";
+import Link from "next/link";
 
-const DESKTOP_MENU = [
+type DesktopMenuElement = {
+  title: {
+    spa: string;
+    eng?: string;
+  };
+  icon: any;
+  path: string;
+};
+
+const DESKTOP_MENU: DesktopMenuElement[] = [
   {
     title: {
       spa: "Proyectos",
     },
+    icon: <Fingerprint />,
     path: "/proyects",
   },
   {
     title: {
       spa: "Sobre Mi",
     },
+    icon: <AboutIcon />,
     path: "/about",
   },
   {
     title: {
       spa: "Contacto",
     },
+    icon: <EmailIcon />,
     path: "/contact",
-  },
-  {
-    title: {
-      spa: "Ingles",
-    },
   },
 ];
 
@@ -31,10 +43,20 @@ const DesktopMenu = () => {
   return (
     <nav className="absolute top-0 w-screen">
       <ul className="flex w-full flex-row justify-evenly px-80 py-16 font-SansationLg">
-        <li className="text-2xl">Proyectos</li>
-        <li className="text-2xl">Sobre Mi</li>
-        <li className="text-2xl">Email</li>
-        <li className="text-2xl">Ingles</li>
+        {DESKTOP_MENU.map((element, index) => (
+          <li key={`${index}-${element.path}`}>
+            <Link href={element.path} className="flex text-2xl">
+              {element.icon}
+              <p className="ml-2 border-l-2 border-black pl-2">
+                {element.title.spa}
+              </p>
+            </Link>
+          </li>
+        ))}
+        <li className="flex text-2xl">
+          <LanguajeIcon />
+          <p className="ml-2 border-l-2 border-black pl-2">Ingles</p>
+        </li>
       </ul>
     </nav>
   );
