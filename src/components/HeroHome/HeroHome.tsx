c815e22a985d2c77004bf645363a24b93f5cc781
Roy Lopez <ryhsu2@gmail.com>
2024-06-12 17:04:31 -0300

    refactor: Add lazy loading for images in HeroHome component

--- src/components/HeroHome/HeroHome.tsx
+++ src/components/HeroHome/HeroHome.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import ChangingWord from "../ChangingWord/ChangingWord";
 import { useSearchParams } from "next/navigation";
 
@@ -9,35 +9,39 @@ const HeroHome = () => {
 
   if (!eng) {
     return (
+      <Suspense>
+        <div className="flex flex-col justify-center *:font-roboto *:text-8xl lg:w-[750px]">
+          <div className="flex justify-start">
+            <p>El</p>
+            <p>&nbsp;</p>
+            <p className="font-extralight">diseño</p>
+            <p>&nbsp;</p>
+            <p className="font-roboto">es una</p>
+          </div>
+          <div className="flex justify-start">
+            <p>forma de</p>
+            <p>&nbsp;</p>
+            <ChangingWord />
+          </div>
+        </div>
+      </Suspense>
+    );
+  }
+  return (
+    <Suspense>
       <div className="flex flex-col justify-center *:font-roboto *:text-8xl lg:w-[750px]">
         <div className="flex justify-start">
-          <p>El</p>
+          <p>Design</p>
           <p>&nbsp;</p>
-          <p className="font-extralight">diseño</p>
-          <p>&nbsp;</p>
-          <p className="font-roboto">es una</p>
+          <p className="font-extralight">is a</p>
         </div>
         <div className="flex justify-start">
-          <p>forma de</p>
+          <p>form of</p>
           <p>&nbsp;</p>
           <ChangingWord />
         </div>
       </div>
-    );
-  }
-  return (
-    <div className="flex flex-col justify-center *:font-roboto *:text-8xl lg:w-[750px]">
-      <div className="flex justify-start">
-        <p>Design</p>
-        <p>&nbsp;</p>
-        <p className="font-extralight">is a</p>
-      </div>
-      <div className="flex justify-start">
-        <p>form of</p>
-        <p>&nbsp;</p>
-        <ChangingWord />
-      </div>
-    </div>
+    </Suspense>
   );
 };
 
