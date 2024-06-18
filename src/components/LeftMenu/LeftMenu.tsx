6a06cc5aeec710dd9099c6127027b7121432bd07
Roy Lopez <ryhsu2@gmail.com>
2024-06-17 21:46:17 -0300

    refactor: Update ProyectsCarrousell component to use lazy loading for images

--- src/components/LeftMenu/LeftMenu.tsx
+++ src/components/LeftMenu/LeftMenu.tsx
@@ -1,20 +1,35 @@
+"use client";
 import cx from "@/app/utils/classesReducer";
-import Link from "next/link";
 import TextTransition from "../RQButton/RQButton";
 import SocialLinks from "../SocialLinks/SocialLinks";
 import RetainQueryLink from "../RetainQueryLink/RetainQueryLink";
+import { usePathname } from "next/navigation";
 
 type LeftMenuProps = {
   className?: string;
 };
 
 const LeftMenu = ({ className }: LeftMenuProps) => {
+  const path = usePathname();
+  const isHome = path === "/";
+
+  const isBrowser = () => typeof window !== "undefined";
+  function scrollToTop() {
+    if (!isBrowser()) return;
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
   return (
     <div className={cx("w-200 flex h-full flex-col pl-4", className)}>
       <div className="grow">
-        <RetainQueryLink href="/">
-          <TextTransition className="mt-28 font-roboto text-2xl font-bold" />
-        </RetainQueryLink>
+        {isHome ? (
+          <button onClick={scrollToTop} title="Scroll to Top">
+            <TextTransition className="mt-28 font-roboto text-2xl font-bold" />
+          </button>
+        ) : (
+          <RetainQueryLink href="/">
+            <TextTransition className="mt-28 font-roboto text-2xl font-bold" />
+          </RetainQueryLink>
+        )}
       </div>
       <SocialLinks className="mb-20" />
     </div>
---
+++ src/components/ProyectsCarrousell/ProyectsCarrousell.tsx
@@ -0,0 +1,72 @@
+import app_home_background from "/public/assets/app_home_background.png";
+import mercado_home_background from "/public/assets/mercado_home_background.png";
+import paella_background from "/public/assets/paella_background.png";
+import madre_dachshund_background from "/public/assets/madre_dachshund_background.png";
+import Image from "next/image";
+
+const PROYECTS = [
+  {
+    number: "1",
+    title: "App Home",
+    detail:
+      "App Home es tu llave para transformar cualquier dispositivo en un hogar inteligente.",
+    year: "2022",
+    image: app_home_background,
+  },
+  {
+    number: "2",
+    title: "Mercado Home",
+    detail:
+      "Mercado Home es un motor de búsqueda de propiedades que compara precios de diferentes sitios web.",
+    year: "2020",
+    image: mercado_home_background,
+  },
+  {
+    number: "3",
+    title: "Paella",
+    detail: "Paella es una Pagina web Responsive.",
+    year: "2020",
+    image: paella_background,
+  },
+  {
+    number: "4",
+    title: "Madre Dachshund",
+    detail:
+      "Madre Dachshund es una pagina web responsive creada para re dirigir el flujo de usuarios desde instagram.",
+    year: "2023",
+    image: madre_dachshund_background,
+  },
+];
+
+const ProyectsCarrousell = () => {
+  return (
+    <>
+      {PROYECTS.map((proyect, index) => (
+        <div
+          className="grid h-full w-full grid-cols-6 items-center justify-center bg-white/30"
+          key={proyect.number}
+        >
+          <div className="col-span-2 grid h-full grid-cols-6">
+            <div className="col-span-3 col-start-3 flex h-1/3 flex-col gap-4 self-center *:font-roboto">
+              <p className="text-7xl">{proyect.number}</p>
+              <p className="text-4xl font-bold">{proyect.title}</p>
+              <p className="text-2xl font-extralight">{proyect.detail}</p>
+              <p className="text-xl">{proyect.year}</p>
+            </div>
+          </div>
+          <div className="col-span-4 h-full py-10">
+            <div className="relative h-full w-full">
+              <Image
+                src={proyect.image}
+                alt="Picture of the author"
+                fill={true}
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default ProyectsCarrousell;
