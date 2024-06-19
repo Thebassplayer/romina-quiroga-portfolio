d4d9dcb3418d36bc797b997a5d86af042fdc72ed
Roy Lopez <ryhsu2@gmail.com>
2024-06-19 17:43:02 -0300

    refactor: Update AboutHero and EmailIcon components to use consistent strokeWidth property
    Update the AboutHero and EmailIcon components to use the consistent strokeWidth property instead of the deprecated stroke-width property. This change ensures consistency in the codebase and improves maintainability.

--- src/components/Icons/EmailIcon.tsx
+++ src/components/Icons/EmailIcon.tsx
@@ -13,14 +13,14 @@ const EmailIcon = (props: SVGProps<SVGSVGElement>) => {
       <path
         d="M24.8438 5.625H5.15625C3.86183 5.625 2.8125 6.67433 2.8125 7.96875V22.0312C2.8125 23.3257 3.86183 24.375 5.15625 24.375H24.8438C26.1382 24.375 27.1875 23.3257 27.1875 22.0312V7.96875C27.1875 6.67433 26.1382 5.625 24.8438 5.625Z"
         stroke="black"
-        stroke-width="2"
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M6.5625 9.375L15 15.9375L23.4375 9.375"
         stroke="black"
-        stroke-width="2"
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
