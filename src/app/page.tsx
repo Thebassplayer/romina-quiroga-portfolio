8ba50604d27ece325425eeb469689a758d10f343
Roy Lopez <ryhsu2@gmail.com>
2024-06-19 18:21:01 -0300

    refactor: Integrate desktop menu feature and improve layout structure
    Update file paths and component imports to integrate the desktop menu feature. Renamed the "about/page.tsx" file to "(desktopMenu)/about/page.tsx" and updated the import statements accordingly. Added a new file "(desktopMenu)/layout.tsx" to serve as the layout component for the desktop menu. Also, updated the "layout.tsx" file to remove the desktop menu component and adjusted the layout structure. These changes enable the implementation of the desktop menu functionality and improve the overall user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,19 @@
 import HeroHome from "@/components/HeroHome/HeroHome";
 import { Suspense } from "react";
-import Proyects from "./(desktopMenu)/proyects/page";
 import ProyectsCarrousell from "@/components/ProyectsCarrousell/ProyectsCarrousell";
 import DesktopMenu from "@/components/DesktopMenu/DesktopMenu";
 export default function Home() {
   return (
-    <>
-      <section className="h-screen w-screen overflow-y-scroll">
-        <div className="flex h-full w-full  flex-col items-center">
-          <DesktopMenu className="z-50" />
-          <div className="flex grow justify-center">
-            <Suspense>
-              <HeroHome />
-            </Suspense>
-          </div>
+    <section className="h-screen w-screen overflow-y-scroll">
+      <div className="flex h-full w-full  flex-col items-center">
+        <DesktopMenu className="z-50" />
+        <div className="flex grow justify-center">
+          <Suspense>
+            <HeroHome />
+          </Suspense>
         </div>
-        <ProyectsCarrousell />
-      </section>
-    </>
+      </div>
+      <ProyectsCarrousell />
+    </section>
   );
 }
