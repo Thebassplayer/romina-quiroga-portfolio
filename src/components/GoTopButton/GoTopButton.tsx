fec30124f4acbeb1b4052ab2174ee583b6644e0e
Roy Lopez <ryhsu2@gmail.com>
2024-06-20 12:24:07 -0300

    refactor: Add border to GoTopButton for improved visibility

--- src/components/GoTopButton/GoTopButton.tsx
+++ src/components/GoTopButton/GoTopButton.tsx
@@ -24,7 +24,7 @@ const GoTopButton = ({ topRef }: GoTopButtonProps) => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, transition: { duration: 0.3 } }}
           exit={{ opacity: 0, transition: { duration: 0.3 } }}
-          className="fixed bottom-10 right-10 bg-black p-4 text-white *:font-roboto"
+          className="fixed bottom-10 right-10 border border-white bg-black p-4 text-white *:font-roboto"
         >
           Go Top
         </motion.button>
