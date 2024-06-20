1dfeaf007d8fa15b52752b5c77a4a722cd2ea219
Roy Lopez <ryhsu2@gmail.com>
2024-06-20 14:58:40 -0300

    refactor: Remove unused useRef in Layout component

--- src/app/(desktopMenu)/layout.tsx
+++ src/app/(desktopMenu)/layout.tsx
@@ -1,17 +1,15 @@
 "use client";
 import DesktopMenu from "@/components/DesktopMenu/DesktopMenu";
 import GoTopButton from "@/components/GoTopButton/GoTopButton";
-import { useRef } from "react";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: LayoutProps) => {
-  const topRef = useRef<HTMLDivElement | null>(null);
   return (
     <>
-      <div ref={topRef} className="flex h-screen w-full flex-col items-center">
+      <div className="flex h-screen w-full flex-col items-center">
         <DesktopMenu />
         <div className="justify-cente mt-12 flex w-full grow">{children}</div>
       </div>
