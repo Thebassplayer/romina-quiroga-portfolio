9e93c86425590c0673e41c0de472bd1ea829802f
Roy Lopez <ryhsu2@gmail.com>
2024-06-26 16:47:13 -0300

    refactor: Update ContactForm styles and add maxlength to input fields
    The ContactForm component in ContactForm.tsx has been refactored to update the styles of the input fields. The class names have been modified to use "text-black" instead of "text-white" for better visibility. Additionally, the "nombre" input field now has a maximum length of 50 characters, and the "email" input field has a maximum length of 100 characters. These changes improve the visual presentation and user experience of the Contact form.

--- src/app/(desktopMenu)/contact/components/ContactForm/ContactForm.tsx
+++ src/app/(desktopMenu)/contact/components/ContactForm/ContactForm.tsx
@@ -5,27 +5,28 @@ const ContactForm = () => {
         Nombre
       </label>
       <input
-        className=" mb-6 rounded-md border bg-white/50 px-4 py-2 text-white focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary"
+        className="mb-6 rounded-md border bg-white/50 px-4 py-2 text-black focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary"
         name="nombre"
         placeholder="Tu nombre"
+        maxLength={50}
         required
       />
       <label className="text-md text-white" htmlFor="email">
         Email
       </label>
       <input
-        className=" mb-6 rounded-md border bg-white/50 px-4 py-2 text-white focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary"
+        className="mb-6 rounded-md border bg-white/50 px-4 py-2 text-black focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary"
         name="email"
+        maxLength={100}
         placeholder="your@mail.com"
         required
       />
       <label className="text-md text-white" htmlFor="email">
         Deja tu mensaje
       </label>
-      <input
-        className=" mb-6 rounded-md border bg-white/50 px-4 py-2 text-white focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary"
+      <textarea
+        className="mb-6 max-h-48 resize-y rounded-md border bg-white/50 px-4 py-2 text-black focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary"
         name="email"
-        type="textarea"
         placeholder="Tu mensaje"
         required
       />
