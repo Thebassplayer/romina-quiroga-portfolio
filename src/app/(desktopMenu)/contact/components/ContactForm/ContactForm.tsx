0104cfda2cbb6ddc09e09cf18d9d9ab02d097db5
Roy Lopez <ryhsu2@gmail.com>
2024-06-27 14:48:02 -0300

    refactor: Update ContactForm language based on query parameter
    Update the ContactForm component in ContactForm.tsx to dynamically change the language based on the "eng" query parameter. If the "eng" parameter is set to "true", the form will display in English, otherwise it will display in Spanish. This enhancement improves the user experience by allowing users to select their preferred language for the contact form.

--- src/app/(desktopMenu)/contact/components/ContactForm/ContactForm.tsx
+++ src/app/(desktopMenu)/contact/components/ContactForm/ContactForm.tsx
@@ -4,9 +4,31 @@ import { contactFormSchema } from "@/schemas/contactForm.schema";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useEffect } from "react";
 import { toFormikValidationSchema } from "zod-formik-adapter";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const CONTACT_FORM_TEXTS = {
+  esp: {
+    nameInput: "Nombre",
+    namePlaceholder: "Tu nombre",
+    emailInput: "Email",
+    emailPlaceholder: "tu@mail.com",
+    messageInput: "Mensaje",
+    messagePlaceholder: "Tu mensaje",
+  },
+  eng: {
+    nameInput: "Name",
+    namePlaceholder: "Your name",
+    emailInput: "Email",
+    emailPlaceholder: "your@email.com",
+    messageInput: "Message",
+    messagePlaceholder: "Your message",
+  },
+};
 
 const ContactForm = () => {
+  const searchParams = useSearchParams();
+  const eng = searchParams.get("eng") === "true" ? true : false;
+  const text = eng ? CONTACT_FORM_TEXTS.eng : CONTACT_FORM_TEXTS.esp;
   const { subscribe, loading, success, error, contactButtonValue } =
     useContact();
 
@@ -27,12 +49,12 @@ const ContactForm = () => {
         <Form className="animate-in text-foreground flex w-1/2 flex-col justify-center gap-2 p-4 *:font-roboto">
           <div className="mb-6 flex w-full flex-col">
             <label className="text-md text-white" htmlFor="name">
-              Nombre
+              {text.nameInput}
             </label>
             <Field
               className="rounded-md border bg-white/50 px-4 py-2 text-black focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary"
               name="name"
-              placeholder="Tu nombre"
+              placeholder={text.namePlaceholder}
               maxLength={50}
             />
             <ErrorMessage
@@ -44,13 +66,13 @@ const ContactForm = () => {
 
           <div className="mb-6 flex w-full flex-col">
             <label className="text-md text-white" htmlFor="email">
-              Email
+              {text.emailInput}
             </label>
             <Field
               className="rounded-md border bg-white/50 px-4 py-2 text-black focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary"
               name="email"
               maxLength={100}
-              placeholder="your@mail.com"
+              placeholder={text.emailPlaceholder}
             />
             <ErrorMessage
               name="email"
@@ -61,13 +83,13 @@ const ContactForm = () => {
 
           <div className="mb-6 flex w-full flex-col">
             <label className="text-md text-white" htmlFor="message">
-              Deja tu mensaje
+              {text.messageInput}
             </label>
             <Field
               as="textarea"
               className="max-h-48 resize-y rounded-md border bg-white/50 px-4 py-2 text-black focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary"
               name="message"
-              placeholder="Tu mensaje"
+              placeholder={text.messagePlaceholder}
             />
             <ErrorMessage
               name="message"
