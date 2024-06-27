895d2c026045bc67817bac946fb6b41a79c26e26
Roy Lopez <ryhsu2@gmail.com>
2024-06-27 15:37:48 -0300

    refactor: Add lazy loading for ContactForm component
    Improve the performance of the ContactForm component by implementing lazy loading using the Suspense component from React. This change delays the rendering of the ContactForm until it is actually needed, resulting in faster page load times. This enhancement enhances the user experience by optimizing the loading of the contact form.

--- src/app/(desktopMenu)/contact/page.tsx
+++ src/app/(desktopMenu)/contact/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { Suspense } from "react";
 import ContactForm from "./components/ContactForm/ContactForm";
 
 const Contact = () => {
   return (
     <div className="flex h-full w-full items-start justify-center pt-20">
-      <ContactForm />
+      <Suspense>
+        <ContactForm />
+      </Suspense>
     </div>
   );
 };
